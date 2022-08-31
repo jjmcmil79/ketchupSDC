b2635db6f0448fb4b37b15561ff788a492bcea95
Joshua McMiller <jjmcmi113r@gmail.com>
2022-08-31 14:44:59 -0500

    updated buildspec

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
 npm start
-# pm2 start npm --name "reactapp" -- start
-# pm2 startup
-# pm2 save
-# pm2 restart all
+pm2 start npm --name "reactapp" -- start
+pm2 startup
+pm2 save
+pm2 restart all
